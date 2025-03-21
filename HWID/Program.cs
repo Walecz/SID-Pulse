af50ec53a548a2686b8f0067a02fa579f1aec567
Administrator <Administrator@WIN-OLE0J74H5CE>
2025-03-21 18:21:01 +0100

    Add project files.

---
+++ HWID/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetHWID());
+            Console.ReadKey();
+        }
+        public static string GetHWID()
+        {
+            return System.Security.Principal.WindowsIdentity.GetCurrent().User.Value;
+        }
+    }
+}
